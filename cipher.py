e636abd5e53dcddf8a7ac327c5fc669ade1f7e7e
19Artem <149903906+19Artem@users.noreply.github.com>
2024-01-06 20:59:01 +0000

    cipher done

--- cipher.py
+++ cipher.py
@@ -1,9 +1,10 @@
 message = input("enter your message: ")
 key = input("enter your key:")
-
+letters = list()
 for char in message:
     new_char = ord(char) + int(key)
-    print(char, ord(char), new_char)
-      
+    print(char, ord(char), new_char, chr(new_char))
+    letters.append(chr (new_char))
 
-print(message, key)
+phrase = "". join(letters)
+print(message, key,phrase)
